dd8f558a6739b9e9f13d0f13dd1e60a1ac5102e5
Ashish Kandpal <63722194+Ashishkandpal@users.noreply.github.com>
2023-02-02 15:31:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+
+
+//create the functions
+function togglePlay(params) {
+	if(video.paused) {
+		video.play();
+	}
+	else{
+		vidoe.pause();
+	}
+}
+
+//add event listeners
+video.addEventListeners("click", togglePlay);
